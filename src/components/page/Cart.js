5a67564d1131f610f59c645d9cf563805d409f96
swati <swatisr.04@gmail.com>
2024-02-20 01:04:33 +0530

    added clar cart functionality

--- src/components/page/Cart.js
+++ src/components/page/Cart.js
@@ -1,28 +1,44 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import Card from "../Card";
-import { clearCart } from "../utils/cartSlice";
-// import store from "../utils/store";
+import { removeItem, clearCart } from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
   const dispatch = useDispatch();
-  const clearFoodItems = dispatch(clearCart());
+
+  const removeFoodItem = (item) => {
+    dispatch(removeItem(item));
+  };
+
+  const clearFoodItem = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <>
       <div className="">
-        {cartItems.map((i) => {
+        {cartItems.map((item) => {
           return (
-            <>
-              <h2 key={i.id}>{i.name}</h2> <h2 key={i.id}>{i.price}</h2>
-              {/* <button className="font-extrabold size-5 h-7 text-xl text-[#eb5e28]">
-                {" "}
-                -{" "}
-              </button> */}
-            </>
+            <div className="flex" key={item.id}>
+              <h2>
+                {item.name} :: Rs.{item.price}
+              </h2>
+              <button
+                className="font-extrabold size-5 h-7 text-sm text-[#eb5e28]"
+                onClick={(item) => removeFoodItem(item)}
+              >
+                Remove
+              </button>
+            </div>
           );
         })}
       </div>
+      <button
+        className="font-extrabold size-5 h-7 text-sm text-[#eb5e28]"
+        onClick={() => clearFoodItem()}
+      >
+        Clear
+      </button>
     </>
   );
 };
